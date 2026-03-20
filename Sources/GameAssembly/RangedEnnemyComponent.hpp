2b4fd556a47caf69f7bf932ccf3abccd0d931f6e
karunanayakedinura <128465343+karunanayakedinura@users.noreply.github.com>
2026-03-20 13:00:34 +0100

    Add Distance Ranged Ennemy

--- Sources/GameAssembly/RangedEnnemyComponent.hpp
+++ Sources/GameAssembly/RangedEnnemyComponent.hpp
@@ -9,8 +9,12 @@ public:
 
 protected:
     void Attack(float deltaTime) override;
+    void Inspect() override;
 
 private:
     float m_ShootCooldown = 1.5f;
     float m_Timer = 0.0f;
+
+    float m_MinDistance = 6.0f; 
+    float m_MaxDistance = 12.0f; 
 };
